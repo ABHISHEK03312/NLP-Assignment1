1d84afa8792cd5ab6538842f274a9dbbb9d4edcd
Kiran Bodipati <kiranbodipati@gmail.com>
2021-10-23 02:32:56 +0800

    Basic app

--- Application/app.py
+++ Application/app.py
@@ -5,11 +5,26 @@ import pandas as pd
 app = Flask(__name__)
 # CORS(app)
 
+def summarise():
+    summary="This is a dummy summary, Replace with the extracter function"
+    return summary
+
+def avg_rating(df, biz):
+    biz_df=df[df['business_id']==biz]
+    if len(biz_df!=0):
+        return sum(biz_df['stars'])/len(biz_df)
+
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 def search_biz():
     data = pd.read_csv("reviewSelected100.csv")
-    selected_biz=request.form["Business"]
-    return render_template("home.html", bid=list(data.business_id), selected_biz=selected_biz)
+    selected_biz=request.form.get("Business")
+    summary =summarise()
+    rating=avg_rating(data, selected_biz)
+    return render_template("home.html", bid=list(data.business_id.unique()), selected_biz=selected_biz, summary=summary, avg_rating=rating)
+
 
 if __name__=="__main__":
     app.run(debug=True)
